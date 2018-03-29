d313a1a07b30094a70229011d3e10f27fa40c345
sanaehururu1200 <sanaehururu1200@gmail.com>
2018-03-29 20:57:20 +0900

    delete console.log

--- app/javascript/mastodon/components/status_content.js
+++ app/javascript/mastodon/components/status_content.js
@@ -126,7 +126,6 @@ export default class StatusContent extends React.PureComponent {
     let lat = status.get('lat');
     let lon = status.get('lon');
     let content;
-    console.log(lat);
     if (lat && lon) {
       map = `<a href='https://maps.google.co.jp/maps?q=${lat},${lon}&target='_blank'><img src='https://maps.googleapis.com/maps/api/staticmap?center=${lat}%2C${lon}&markers=color%3Ared%7Csize%3Amid%7C${lat}%2C${lon}&zoom=15&size=200x100&sensor=false&key=AIzaSyAS_RnMcc5glB_ufybY-mj-8fQOHrZEF6M'></a>`;
     }
