9f7afef9b37d473f53f529602e660b2472d7931f
sanaehururu1200 <sanaehururu1200@gmail.com>
2018-03-30 10:44:58 +0900

    デフォルトカラーを選択可能に

--- app/javascript/mastodon/features/compose/components/compose_form.js
+++ app/javascript/mastodon/features/compose/components/compose_form.js
@@ -294,7 +294,7 @@ export default class ComposeForm extends ImmutablePureComponent {
               </div>
               { this.state.displayColorPicker ? <div style={ styles.popover }>
                 <div style={ styles.cover } onClick={ this.handleClose }/>
-                <SketchPicker color={ this.state.color } onChange={ this.handleChangeColor } />
+                <SketchPicker color={this.state.color} onChange={this.handleChangeColor} presetColors={['#D0021B', '#F5A623', '#F8E71C', '#8B572A', '#7ED321', '#417505', '#BD10E0', '#9013FE', '#4A90E2', '#50E3C2', '#B8E986', '#000000', '#4A4A4A', '#9B9B9B', '#FFFFFF', '#282c37']} />
               </div> : null }
             </div>
             <IconButton icon='map-marker' title='位置情報を追加' inverted disabled={false} onClick={this.handleOpenModal} size={20} />
